15113d368418e6a218af016fbba719ff8dca27dd
Jami Hämäläinen <jami.hamala@gmail.com>
2025-02-14 12:44:13 +0200

    logging in now switches to home page

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,9 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Login from './components/Login/Login'
 import Home from './components/Home'
 
-const App = () => {
+import { useEffect } from 'react'
 
+const App = () => {
   return (
     <BrowserRouter>
       <Routes>
--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,7 +1,17 @@
 import '../../styles/Login.css'
 import LoginForm from './LoginForm'
+import { useNavigate } from 'react-router-dom'
+import { useEffect } from 'react'
+import { useSelector } from 'react-redux'
 
 const Login = () => {
+  const navigate = useNavigate()
+  const user = useSelector(state => state.user)
+
+  useEffect(() => {
+    if (user) navigate('/')
+  }, [user])
+
   return (
     <div className='login-container'>
       <h2>Log in</h2>
